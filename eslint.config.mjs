437703c24805bd7c4aa973af3f0d42294029a20d
fikrianamm <fikritaufiqulanam1@gmail.com>
2024-07-31 21:08:57 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+    },
+}, ...compat.extends(
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+).map(config => ({
+    ...config,
+    files: ["**/*.ts", "**/*.tsx"],
+})), {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    languageOptions: {
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+            project: "tsconfig.json",
+        },
+    },
+    rules: {
+        "no-console": "off",
+    },
+}];
